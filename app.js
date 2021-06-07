30eceecbcdd64740c8a1f295be04cf488841808a
tabishismail <tabish554@gmail.com>
2021-06-07 23:05:43 +0500

    Chapter # 39 to 42 Assignment # 2 done

---
+++ app.js
@@ -0,0 +1,25 @@
+// Chapter # 39 to 42 
+
+// Assignment # 1
+
+// function pow(a,b){
+//  var res=Math.pow(a,b);
+//  console.log(res);
+// }
+// pow(5,2);
+
+
+// Assignment # 2
+
+// var year = +prompt("Enter a year:");
+
+// function check(year) {
+
+//     var leap = new Date(year, 1, 29).getDate() === 29;
+//     if (leap) {
+//         console.log(year + ' is a leap year');
+//     } else {
+//         console.log(year + ' is not a leap year');
+//     }
+// }
+// check(year);
